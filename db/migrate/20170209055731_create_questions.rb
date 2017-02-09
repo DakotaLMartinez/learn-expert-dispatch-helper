7eceb0dc763d714a2c671c73c17fd0857d8cc783
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-02-08 22:19:49 -0800

    adds questions

---
+++ db/migrate/20170209055731_create_questions.rb
@@ -0,0 +1,16 @@
+class CreateQuestions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :questions do |t|
+      t.datetime :time_asked
+      t.string :type
+      t.string :github_username
+      t.string :email
+      t.string :question_link
+      t.string :learn_expert
+      t.datetime :start_share
+      t.datetime :end_share
+      t.string :notes
+      t.timestamps
+    end
+  end
+end
