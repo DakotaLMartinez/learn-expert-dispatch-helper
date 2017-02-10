0e63f6d415584b5ff04e6576ab6f0d8e405e481e
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-02-09 16:05:59 -0800

    adds seed data for 7 questions

---
+++ db/seeds.rb
@@ -0,0 +1,56 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+questions = Question.create([
+  { 
+    question_type: 'Chat-Dispatch',
+    asker_name: 'Eric Levy',
+    github_username: 'levyeric10',
+    learn_expert: 'Traci',
+    question_link: 'https://learn.co/lessons/12818?batch_id=306&track_id=12615&question_id=48638' 
+  },
+  { 
+    question_type: 'Chat-Dispatch', 
+    asker_name: 'Henry Zhang',
+    github_username: 'KingHenryJr', 
+    learn_expert: 'Traci',
+    question_link: 'https://learn.co/lessons/19394?batch_id=460&track_id=19381&question_id=48639' 
+  },
+  { 
+    question_type: 'SS', 
+    asker_name: 'Liah Wallace',
+    github_username: 'liahwallace1',
+    learn_expert: 'pickledyamsman',
+    question_link: 'https://learn.co/lessons/14102?batch_id=306&track_id=12615&question_id=48640' 
+  },
+  { 
+    question_type: 'Chat-Dispatch', 
+    email: 'pvikram1@binghamton.edu',
+    learn_expert: 'Traci',
+    question_link: 'https://learn.co/lessons/19012?batch_id=436&track_id=18316&question_id=48641' 
+  },
+  { 
+    question_type: 'SS', 
+    asker_name: 'Pablo Lee',
+    github_username: 'PabloyotoLee', 
+    learn_expert: 'tibsar',
+    question_link: 'https://learn.co/lessons/19413?batch_id=460&track_id=19381&question_id=48642' 
+  },
+  { 
+    question_type: 'Chat-Dispatch', 
+    asker_name: 'Phillip Fine',
+    github_username: 'PhillipFine', 
+    learn_expert: 'Traci',
+    question_link: 'https://learn.co/lessons/19413?batch_id=460&track_id=19381&question_id=48643' 
+  },
+  { 
+    question_type: 'SS', 
+    email: 'tricia.dros@gmail.com', 
+    learn_expert: 'pickledyamsman',
+    question_link: 'https://learn.co/lessons/19012?batch_id=436&track_id=18316&question_id=48644' 
+  }
+])
